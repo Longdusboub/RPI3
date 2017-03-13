45c2b56dc148b93752c4904c5944cb3900fc726a
Alexandre Meunier <ameunier@mail.ks.int>
2017-03-13 10:36:28 +0100

    Remove useless line in JTAG Aarch64

--- JTAG/RPI3/Aarch64/test.c
+++ JTAG/RPI3/Aarch64/test.c
@@ -4,8 +4,6 @@
  */
 
 #include "gpio.c"
-#include "coretimer.c"
-#include "interrupt.c"
 
 void switchLED(void)
 {
@@ -32,14 +30,8 @@ int main( void )
 {
 	gpio_set_mode(21, GPIO_FUNCTION_OUT);
 	init_jtag();
-	ct_init_timer(0x8FFF);
-	ct_set_increment(1);
-	ct_set_source(CRY);
-	ct_set_prescaler(0x00FFFFFF);
-	ct_set_ctrl_reg(0, 1);
-	while (1) if (ct_int(0, 1) == 1)
+	while (1)
 	{
-		ct_init_timer(0x8FFF);
 		switchLED();
 	}
 	return(0);
